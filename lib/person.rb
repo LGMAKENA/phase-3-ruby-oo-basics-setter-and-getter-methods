0ec0d317afcc7a9cc05a3baca926e019927e8eec
lgmakena <linetmakenag@gmail.com>
2023-07-07 05:46:00 +0300

    wk2

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
-
-end
+    def name
+      @name
+    end
+  
+    def name=(person_name)
+      @name = person_name
+    end
+  
+    def job
+      @job
+    end
+  
+    def job=(person_job)
+      @job = person_job
+    end
+  end
+  
